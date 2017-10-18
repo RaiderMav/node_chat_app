d77941992cb2611a553b3eb1e9911e5a150a161e
RaiderMav <raidermaverick@gmail.com>
2017-10-18 17:44:12 -0500

    Emit new message on createMessage

--- public/js/index.js
+++ public/js/index.js
@@ -4,12 +4,6 @@ socket.on('connect', function () {
   console.log('Connected to server')
 })
 
-socket.emit('createMessage', {
-  from: 'Helen Mirren',
-  text: 'Yup, I wanna bang you!'
-
-})
-
 socket.on('newMessage', function (message) {
   console.log('newMessage', message)
 })
